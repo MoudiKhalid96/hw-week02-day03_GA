cd9b939fc0a791767f9a0b5fc14306b607394bff
moudikhalid96 <moudi_khalid96@hotmail.com>
2021-03-18 18:25:17 +0300

    hw completed

--- Dancing_Cats solution/JS/main.js
+++ Dancing_Cats solution/JS/main.js
@@ -1,40 +1,16 @@
 let catImage = document.getElementsByTagName('img')[0];
 
-let catURL = catImage.src;
-
 catImage.style.position = "absolute";
-catImage.style.left = "0 px";
+var moveBy = 1;
 
-
-// function catWalk(){
-//     catImage.style.position = "relative";
-//     catImage.style.left = "10 px";
-// }
-
-function catWalk(){
-
-    let pos = '0px';
-   
-    pos++;
-    catImage.style.left = pos; 
-
-    setInterval(function(){
-        catImage.style.position = "absolute";
-        catImage.style.left = '10 px';
-
-    }, 10);
-
-
-
-
-    /*
-    let pos = 0%;
-   
-    pos += 1%; 
-    elem.style.left = pos; 
-    */
+function catWalk() {
+    setTimeout(() => {
+        if (moveBy == "80") {
+            return;
+        } else {
+            catImage.style.left = moveBy + "%";
+            moveBy++;
+            catWalk();
+        }
+    }, 50)
 }
-
-
-
-
